63b50cb8ef05db1baa5f885988fe9b0d5baef5f2
selphcode <selphcode@users.noreply.github.com>
2017-07-15 09:51:53 -0700

    Update module5.js

--- Pages/Modules/module5.js
+++ Pages/Modules/module5.js
@@ -1,1 +1,11 @@
+function myFunction() {
+    var txt;
+    var person = prompt("Please enter your name:", "YOUR NAME HERE");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
 
