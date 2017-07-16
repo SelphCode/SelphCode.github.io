09722a91a341f22a66960d8fb96ecddd94128765
selphcode <selphcode@users.noreply.github.com>
2017-07-15 21:28:54 -0700

    modify mod6

--- Pages/Modules/module6.js
+++ Pages/Modules/module6.js
@@ -1,7 +1,8 @@
+var msg = '<p><b>Week 6 - Weekly Assignment </b>' + '<br />';
 
 // Create a variable called msg to hold a message that will be shown on the page
 // Find the title of the document, and put this in the msg variable
-var msg = '<p><b>page title: </b>' + document.title + '<br />';
+    msg += '<p><b>page title: </b>' + document.title + '<br />';
 // Find the URL of the document and add it to the msg variable
 msg += '<b>page address: </b>' + document.URL + '<br />';
 // Find the date the document was last modified and add it to the msg variable
