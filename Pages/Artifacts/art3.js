cd3abb249988f6fced8cf29db17a6943bab026d3
selphcode <selphcode@users.noreply.github.com>
2017-07-20 22:47:09 -0700

    reverting

--- Pages/Artifacts/art3.js
+++ Pages/Artifacts/art3.js
@@ -4,44 +4,11 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-    upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-    var rLine ="";
-    for (i=1;i<=pHeight;i++){
-        rLine +="<p>";
-//fill in the left side of every row with spaces or blanks
-        for (x=1; x<=pHeight-i;x++){
-            rLine+="-";
-        rline+="<span class='space'>"+
-               pSymbol +"</span>";}
-//for (x=1; x<=pHeight-i;x++){
-  //  rLine+="-";
-}
-//Create each line on the Rhombus
-        for(j=x;j<=pHeight;j++){
-
-//Is the position even or odd so we change the color
-            if (j%2)
-//even
-                rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-            else
-//odd
-                rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-        }
-        rLine +="</p>";
-// console.log(rLine);
-
-    }
-
-    document.getElementById("upLeft").innerHTML = rLine;
-}
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
     for (i=0;i<pHeight;i++){
@@ -64,30 +31,6 @@ function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     }
 
     document.getElementById("upRight").innerHTML = rLine;
-}
-
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-    var rLine ="";
-    for (i=pHeight;i > 0;i--){
-        rLine +="<p>";
-//Create each line on the Rhombus
-        for(j=0;j<i;j++){
-
-//Is the position even or odd so we change the color
-            if (j%2)
-//even
-                rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-            else
-//odd
-                rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-
-        }
-        rLine +="</p>";
-// console.log(rLine);
-
-    }
-
-    document.getElementById("downLeft").innerHTML = rLine;
 }
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -113,3 +56,4 @@ function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 
     document.getElementById("downRight").innerHTML = rLine;
 }
+
