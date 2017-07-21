a7a00710f6a5bb083ee855d58ce1b45ed14db64e
selphcode <selphcode@users.noreply.github.com>
2017-07-20 22:59:30 -0700

    add idea

--- Pages/Artifacts/art3.js
+++ Pages/Artifacts/art3.js
@@ -4,9 +4,41 @@
 // var symbol ="*";
 
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+
+    upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
 
+}
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+    var rLine ="";
+    for (i=1;i<=pHeight;i++){
+        rLine +="<p>";
+
+//fill in the left side of every row with spaces or blanks
+        for (x=1; x<=pHeight-i;x++){
+            rLine +="<span class='space'>" + pSymbol +"</span>";
+        }
+//Create each line on the Rhombus
+        for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+            if (j%2)
+//even
+                rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+            else
+//odd
+                rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+        }
+        rLine +="</p>";
+// console.log(rLine);
+
+    }
+
+    document.getElementById("upRight").innerHTML = rLine;
 }
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
@@ -31,6 +63,30 @@ function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     }
 
     document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+    var rLine ="";
+    for (i=pHeight;i > 0;i--){
+        rLine +="<p>";
+//Create each line on the Rhombus
+        for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+            if (j%2)
+//even
+                rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+            else
+//odd
+                rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+        }
+        rLine +="</p>";
+// console.log(rLine);
+
+    }
+
+    document.getElementById("downRight").innerHTML = rLine;
 }
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
