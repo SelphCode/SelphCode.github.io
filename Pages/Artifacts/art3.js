c66ee1938e2fe727ae98bef04c81cef8c7892f0b
selphcode <selphcode@users.noreply.github.com>
2017-07-23 16:57:53 -0700

    reverted

--- Pages/Artifacts/art3.js
+++ Pages/Artifacts/art3.js
@@ -6,26 +6,23 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
 
     upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
-
     downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-
+    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 
 }
 
 function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
-    for (i=1;i<=pHeight-i;i++){
+    for (i=1;i<=pHeight;i++){
         rLine +="<p>";
 
 //fill in the left side of every row with spaces or blanks
-        for (x=1; x<=pHeight;x++){
+        for (x=1; x<=pHeight-i;x++){
             rLine +="<span class='space'>" + pSymbol +"</span>";
         }
 //Create each line on the Rhombus
-        for(j=x-1;j<=pHeight;j++){
+        for(j=x;j<=pHeight;j++){
 
 //Is the position even or odd so we change the color
             if (j%2)
@@ -94,7 +91,7 @@ function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
 
 function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine ="";
-    for (i=pHeight+i;i > 0;i--){
+    for (i=pHeight;i > 0;i--){
         rLine +="<p>";
 //Create each line on the Rhombus
         for(j=0;j<i;j++){
