4cc04d86293c822105b12568ea8c3e33fb0d365e
selphcode <selphcode@users.noreply.github.com>
2017-08-06 22:08:29 -0700

    add files

--- Pages/Artifacts/slideshow.js
+++ Pages/Artifacts/slideshow.js
@@ -1,4 +1,4 @@
-var myImages =["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg","http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
+var myImages =["../Pages/Artifacts/images/art1.jpeg", "../Pages/Artifacts/images/art2.jpeg","../Pages/Artifacts/images/art3.jpeg","../Pages/Artifacts/images/art4.jpeg","../Pages/Artifacts/images/art5.jpeg"];
 
 
 var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
